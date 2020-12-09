294821f76bc26310fb1efea9f86dd8d44aa82188
Mai Ristioja <mai@ristioja.ee>
2020-12-10 01:32:19 +0200

    Added second activity, chained with mainactivity, UI improvements

--- App/app/src/main/java/ee/ut/gimmefood/MainActivity.kt
+++ App/app/src/main/java/ee/ut/gimmefood/MainActivity.kt
@@ -1,11 +1,14 @@
 package ee.ut.gimmefood
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ee.ut.gimmefood.data.Food
 import ee.ut.gimmefood.shopping.ShoppingAdapter
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val REQUEST_ORDER = 200
     lateinit var database: MockDatabase
     var orderQuantities: MutableMap<Food, Int> = mutableMapOf()
     lateinit var shoppingAdapter: ShoppingAdapter
@@ -22,7 +25,27 @@ class MainActivity : AppCompatActivity() {
 
         my_recyclerview.adapter = shoppingAdapter
 
+        button_place_order.setOnClickListener {
+            val orderFoodIdsList = mutableListOf<Long>()
+            val orderQuantitiesList = mutableListOf<Int>()
+            for ((key, value) in orderQuantities) {
+                orderFoodIdsList.add(key.id)
+                orderQuantitiesList.add(value)
+            }
+
+            val intent = Intent(this, OrderDetailsActivity::class.java)
+            intent.putExtra("ids", orderFoodIdsList.toLongArray())
+            intent.putExtra("quantities", orderQuantitiesList.toIntArray())
+            startActivityForResult(intent, REQUEST_ORDER)
+        }
+
+        notifyDataSetChanged()
+    }
+
+    private fun notifyDataSetChanged() {
+        shoppingAdapter.notifyDataSetChanged()
         updateTotalText()
+        button_place_order.isEnabled = orderQuantities.isNotEmpty()
     }
 
     private fun changeFoodQuantity(food: Food, changeDelta: Int) {
@@ -32,8 +55,7 @@ class MainActivity : AppCompatActivity() {
         } else {
             orderQuantities.put(food, newQuantity)
         }
-        shoppingAdapter.notifyDataSetChanged()
-        updateTotalText()
+        notifyDataSetChanged()
     }
 
     private fun updateTotalText() {
@@ -41,5 +63,19 @@ class MainActivity : AppCompatActivity() {
         for ((food, quantity) in orderQuantities)
             sum += food.price * quantity
         total_textview.text = getString(R.string.total_text, sum)
+    }
+
+    private fun clearQuantities() {
+        orderQuantities.clear()
+        notifyDataSetChanged()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_ORDER) {
+            if (resultCode == RESULT_OK) {
+                clearQuantities()
+            }
+        }
     }
 }
