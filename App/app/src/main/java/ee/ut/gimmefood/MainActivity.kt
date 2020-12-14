aa512e1e9572caa3137d99ea2aa7cb1e9341d3bd
Brandon Autrey <brandon.c.autrey@gmail.com>
2020-12-14 22:36:56 +0200

    trying to get settings to work

--- App/app/src/main/java/ee/ut/gimmefood/MainActivity.kt
+++ App/app/src/main/java/ee/ut/gimmefood/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.MenuItem
 import ee.ut.gimmefood.camera.kotlin.CameraXLivePreviewActivity
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -17,5 +18,15 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, CameraXLivePreviewActivity::class.java))
         }
 
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.your_item_id -> {
+                startActivity(Intent(this, SettingsActivity::class.java))
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
